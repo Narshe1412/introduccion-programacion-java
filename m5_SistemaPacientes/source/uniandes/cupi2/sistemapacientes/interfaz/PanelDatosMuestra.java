78601c8bf5a7ba506b26fed0688e1f3649423c12
Manuel <manuel1412@gmail.com>
2018-03-19 13:09:11 +0000

    Interfaces Datos Muestra y Datos Paciente añadidas

--- m5_SistemaPacientes/source/uniandes/cupi2/sistemapacientes/interfaz/PanelDatosMuestra.java
+++ m5_SistemaPacientes/source/uniandes/cupi2/sistemapacientes/interfaz/PanelDatosMuestra.java
@@ -2,19 +2,125 @@ package uniandes.cupi2.sistemapacientes.interfaz;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
 public class PanelDatosMuestra extends JPanel{
+	// Etiqueta y zona de texto para fecha de la toma de muestra
+	private JLabel labFTomaMuestra;
+	private JTextField txtFTomaMuestra;
+	// Volumen de la muestra
+	private JLabel labVolumenMuestra;
+	private JTextField txtVolumenMuestra;
+	// Volumen de eritrocitos
+	private JLabel labVolumenEritrocitos;
+	private JTextField txtVolumenEritrocitos;
+	// Conteo Leucocitos
+	private JLabel labConteoLeucocitos;
+	private JTextField txtConteoLeucocitos;
+	// Conteo Plaquetas
+	private JLabel labConteoPlaquetas;
+	private JTextField txtConteoPlaquetas;
+	
+	// Check si la muestra se tomó en ayunas
+	private JCheckBox cbAyunas;
+	
+	//Boton y texto para calcular hematocrito
+	private JButton butHematocrito;
+	private JTextField txtHematocrito;
+	//Boton y texto para calcular leucocito
+	private JButton butLeucocito;
+	private JTextField txtLeucocito;
+	
 	/**
 	 * Constructor del panel datos de paciente
 	 */
 	public PanelDatosMuestra() {
-		setLayout(new BorderLayout());
+		setLayout(new GridLayout(6, 4));
 		TitledBorder border = BorderFactory.createTitledBorder("Información muestra");
 		border.setTitleColor(Color.BLUE);
 		setBorder(border);
+		
+		
+		// Creacción de elementos para la interfaz
+		// Fecha Toma de Muestra
+		labFTomaMuestra = new JLabel("Fecha Toma de Muestra: ");
+		txtFTomaMuestra = new JTextField(10);
+		txtFTomaMuestra.setEditable(false);
+		txtFTomaMuestra.setBackground(Color.LIGHT_GRAY);
+		txtFTomaMuestra.setForeground(Color.BLUE);
+		
+		// Check de muestra en ayunas
+		cbAyunas = new JCheckBox("Ayunas");
+		
+		// Volumen de la muestra
+		labVolumenMuestra = new JLabel("Volumen de Muestra: ");
+		txtVolumenMuestra = new JTextField(10);
+		txtVolumenMuestra.setEditable(true);
+		txtVolumenMuestra.setBackground(Color.WHITE);
+		txtVolumenMuestra.setForeground(Color.BLUE);
+		
+		// Volumen de Eritrocitos
+		labVolumenEritrocitos = new JLabel("Volumen Eritrocitos: ");
+		txtVolumenEritrocitos = new JTextField(10);
+		txtVolumenEritrocitos.setEditable(true);
+		txtVolumenEritrocitos.setBackground(Color.WHITE);
+		txtVolumenEritrocitos.setForeground(Color.BLUE);
+		
+		// Boton Calcular Hematocrito
+		butHematocrito = new JButton("Calcular Hematocrito");
+		txtHematocrito = new JTextField(10);
+		txtHematocrito.setEditable(false);
+		
+		// Conteo de Leucocitos
+		labConteoLeucocitos = new JLabel("Conteo de Leucocitos: ");
+		txtConteoLeucocitos = new JTextField(10);
+		txtConteoLeucocitos.setEditable(true);
+		txtConteoLeucocitos.setBackground(Color.WHITE);
+		txtConteoLeucocitos.setForeground(Color.BLUE);
+
+		// Boton Calcular Leucocitos
+		butLeucocito = new JButton("Calcular Leucocito");
+		txtLeucocito = new JTextField(10);
+		txtLeucocito.setEditable(false);
+		
+		// Conteo de Plaquetas
+		labConteoPlaquetas = new JLabel("Conteo de Plaquetas: ");
+		txtConteoPlaquetas = new JTextField(10);
+		txtConteoPlaquetas.setEditable(true);
+		txtConteoPlaquetas.setBackground(Color.WHITE);
+		txtConteoPlaquetas.setForeground(Color.BLUE);
+		
+		add(labFTomaMuestra);
+		add(txtFTomaMuestra);
+		add(new JLabel());
+		add(cbAyunas);
+		add(labVolumenMuestra);
+		add(txtVolumenMuestra);
+		add(new JLabel());
+		add(new JLabel());
+		add(labVolumenEritrocitos);
+		add(txtVolumenEritrocitos);
+		add(butHematocrito);
+		add(txtHematocrito);
+		add(labConteoLeucocitos);
+		add(txtConteoLeucocitos);
+		add(butLeucocito);
+		add(txtLeucocito);
+		add(labConteoPlaquetas);
+		add(txtConteoPlaquetas);
+		add(new JLabel());
+		add(new JLabel());
+		add(new JLabel());
+		add(new JLabel());
+		add(new JLabel());
+		add(new JLabel());
 	}
 }
--- m5_SistemaPacientes/source/uniandes/cupi2/sistemapacientes/interfaz/PanelDatosPaciente.java
+++ m5_SistemaPacientes/source/uniandes/cupi2/sistemapacientes/interfaz/PanelDatosPaciente.java
@@ -2,12 +2,37 @@ package uniandes.cupi2.sistemapacientes.interfaz;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
 public class PanelDatosPaciente extends JPanel{
+	// Etiqueta y zona de texto para nombre
+	private JLabel labNombre;
+	private JTextField txtNombre;
+	// Apellido
+	private JLabel labApellido;
+	private JTextField txtApellido;
+	// Fecha de nacimiento
+	private JLabel labFNacimiento;
+	private JTextField txtFNacimiento;
+	// Sexo
+	private JLabel labSexo;
+	private JTextField txtSexo;
+	
+	// Imagen
+	private JLabel labImagen;
+	
+	//Boton y texto para calcular la edad
+	private JButton butEdad;
+	private JTextField txtEdad;
+	
+	
 	/**
 	 * Constructor del panel datos de paciente
 	 */
@@ -16,5 +41,69 @@ public class PanelDatosPaciente extends JPanel{
 		TitledBorder border = BorderFactory.createTitledBorder("Datos del paciente");
 		border.setTitleColor(Color.BLUE);
 		setBorder(border);
+		
+		//Panel para mostrar la foto del paciente
+		JPanel panelFoto;
+		
+		//Panel para mostrar la información del paciente
+		JPanel panelInformacion;
+		
+		//Creacción y localización de los panels
+		panelFoto = new JPanel();
+		panelInformacion = new JPanel();
+		
+		add(panelFoto, BorderLayout.EAST);
+		add(panelInformacion, BorderLayout.CENTER);
+		
+		// Creacción de elementos para la interfaz
+		// Nombre
+		labNombre = new JLabel("Nombre: ");
+		txtNombre = new JTextField(15);
+		txtNombre.setEditable(false);
+		txtNombre.setBackground(Color.LIGHT_GRAY);
+		txtNombre.setForeground(Color.BLUE);
+		
+		// Apellido
+		labApellido = new JLabel("Apellido: ");
+		txtApellido = new JTextField(15);
+		txtApellido.setEditable(false);
+		txtApellido.setBackground(Color.LIGHT_GRAY);
+		txtApellido.setForeground(Color.BLUE);
+		
+		// Sexo
+		labSexo = new JLabel("Sexo: ");
+		txtSexo = new JTextField(2);
+		txtSexo.setEditable(false);
+		txtSexo.setBackground(Color.LIGHT_GRAY);
+		txtSexo.setForeground(Color.BLUE);
+		
+		// Fecha de nacimiento
+		labFNacimiento = new JLabel("Fecha de nacimiento: ");
+		txtFNacimiento = new JTextField(10);
+		txtFNacimiento.setEditable(false);
+		txtFNacimiento.setBackground(Color.LIGHT_GRAY);
+		txtFNacimiento.setForeground(Color.BLUE);
+		
+		// Edad
+		butEdad = new JButton("Calcular Edad");
+		txtEdad = new JTextField(10);
+		txtEdad.setEditable(false);
+		
+		// Añadir elementos al panel
+		panelInformacion.setLayout(new GridLayout(6,2));
+		panelInformacion.add(labNombre);
+		panelInformacion.add(txtNombre);
+		panelInformacion.add(labApellido);
+		panelInformacion.add(txtApellido);
+		panelInformacion.add(labSexo);
+		panelInformacion.add(txtSexo);
+		panelInformacion.add(labFNacimiento);
+		panelInformacion.add(txtFNacimiento);
+		panelInformacion.add(butEdad);
+		panelInformacion.add(txtEdad);
+
+		// Foto
+		labImagen = new JLabel("FOTO");
+		panelFoto.add(labImagen);
 	}
 }
